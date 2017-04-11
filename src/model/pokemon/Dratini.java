784111aa374e98649fee9bc2674d6a8c3e5c019a
Wan-ZL <zelinwan@email.arizona.edu>
2017-04-11 16:51:15 -0700

    backup

---
+++ src/model/pokemon/Dratini.java
@@ -0,0 +1,30 @@
+package model.pokemon;
+
+import model.Pokemon;
+
+public class Dratini extends Pokemon{
+
+	public Dratini() {
+		super(PokemonType.Dratini, 150, 35, PokemonRarity.COMMON);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void eatBait() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void HitByRock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void chooseAttack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/model/pokemon/Parasect.java
@@ -0,0 +1,30 @@
+package model.pokemon;
+
+import model.Pokemon;
+
+public class Parasect extends Pokemon{
+
+	public Parasect() {
+		super(PokemonType.Paras, 300, 20, PokemonRarity.COMMON);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void eatBait() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void HitByRock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void chooseAttack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
