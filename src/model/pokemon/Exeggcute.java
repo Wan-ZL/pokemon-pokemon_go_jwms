556af7e02de341ad48470cb6d30f11a8d66fc481
Wan-ZL <zelinwan@email.arizona.edu>
2017-04-11 16:44:31 -0700

    backup

---
+++ src/model/pokemon/Exeggcute.java
@@ -0,0 +1,30 @@
+package model.pokemon;
+
+import model.Pokemon;
+
+public class Exeggcute extends Pokemon{
+
+	public Exeggcute() {
+		super(PokemonType.Paras, 200, 30, PokemonRarity.COMMON);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void eatBait() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void HitByRock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void chooseAttack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
