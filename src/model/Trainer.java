8f4b6859542ba18aa7f809daf34784b61d908edf
Wan-ZL <zelinwan@email.arizona.edu>
2017-04-12 14:26:01 -0700

    pokemonbelt

--- src/model/Trainer.java
+++ src/model/Trainer.java
@@ -1,12 +1,26 @@
 package model;
 
+
 public class Trainer {
 
 	private String name;
 	private BackPack pack;
 	private PokemonBelt belt;
 	
+	private static Trainer thisTrainer;
+	
 	public Trainer(String name) {
 		this.name = name;
+	}
+	
+	public static Trainer getTrainerInstance(){		// create instance variable of trainer
+		if(thisTrainer == null){
+			thisTrainer = new Trainer("Trainer Name");
+		}
+		return thisTrainer;
+	}
+	
+	public PokemonBelt getBelt(){					// return the pokemonbelt
+		return belt;
 	}
 }
