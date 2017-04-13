b96fdde292a47e40d45e1ca04101cf5857bfc177
Wan-ZL <zelinwan@email.arizona.edu>
2017-04-12 19:38:09 -0700

    edit trainer

--- src/model/Trainer.java
+++ src/model/Trainer.java
@@ -30,4 +30,20 @@ public class Trainer {
 	public boolean addPokemon(Pokemon pokemon){		// add a pokemon to belt, return true if success
 		return this.belt.addPokemon(pokemon);
 	}
+	
+	public boolean removePokemon(Pokemon pokemon){	// remove a pokemon from belt, if have multi same pokemon, remove the first one.
+		return this.belt.removePokemon(pokemon);
+	}
+	
+	public Pokemon removePokemon(int index){		// remove a pokemon on specific index, return the removed pokemon object.
+		return this.belt.removePokemon(index);
+	}
+	
+	public Pokemon[] getBeltArray(){				// return the whole pokemon on belt, type is array of pokemon
+		return this.belt.getBeltArray();
+	}
+	
+	public int getSize(){							// return the number of pokemon on belt.
+		return this.belt.getSize();
+	}
 }
