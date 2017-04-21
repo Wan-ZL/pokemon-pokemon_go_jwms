d78cd4a68314739e32cc973745cee2785fa25253
Jeremiah Hanson <Jeremiah Hanson>
2017-04-21 13:30:12 -0700

    Added some starter code into battleView

--- src/view/BattleView.java
+++ src/view/BattleView.java
@@ -1,5 +1,19 @@
 package view;
 
-public class BattleView {
+import javax.swing.JPanel;
 
+import model.Trainer;
+
+public class BattleView extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4796227636732972155L;
+
+	private Trainer trainer;
+	
+	public BattleView(Trainer trainer) {
+		this.trainer = trainer;
+	}
 }
