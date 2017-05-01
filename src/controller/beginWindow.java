d260e6c56759a0ceed9a10bb9c41841e34ecf9d0
Stephen Zhu <songzhezhu@email.arizona.edu>
2017-05-01 11:12:51 -0700

    added author names

--- src/controller/beginWindow.java
+++ src/controller/beginWindow.java
@@ -22,7 +22,7 @@ public class beginWindow extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Pokemon Safari Login");
 		JFXPanel fxPanel = new JFXPanel();
-		this.setSize((20 * 11) + 230, (20 * 11) + 70);
+		this.setSize((20 * 11) + 230, (20 * 11) + 100);
 		this.setLocation(100, 100);
 		panel = new LoadingView(this);
 		panel.setSize((20 * 11) + 230, (20 * 11) + 70);
