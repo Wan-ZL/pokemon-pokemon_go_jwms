449cac4c23be070664f70a3db8f94faf3b56fb1d
Stephen Zhu <songzhezhu@email.arizona.edu>
2017-05-02 15:10:11 -0700

    fix conflict error

--- src/controller/pokemonGUI.java
+++ src/controller/pokemonGUI.java
@@ -472,7 +472,7 @@ public class pokemonGUI extends JFrame {
 				 */
 
 
-				if (trainer.getX() == 5) { // map change
+				/*if (trainer.getX() == 5) { // map change
 					if (trainer.getY() == 17 || trainer.getY() == 18) {
 						System.out.println("switch");
 						trainer.setMapNum(2);
@@ -489,7 +489,7 @@ public class pokemonGUI extends JFrame {
 						}
 					}
 				}
-
+*/
 				/*
 				 * if (trainer.getX() == 5) { // map change if (trainer.getY()
 				 * == 17 || trainer.getY() == 18) {
