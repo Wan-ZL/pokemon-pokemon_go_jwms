8219ee4232a0372dc0a357c24456077a7c86440b
mutianyan <mutianyan@dhcp-10-134-237-247.uawifi.arizona.edu>
2017-05-02 16:54:54 -0700

    using the max-potion

--- src/controller/pokemonGUI.java
+++ src/controller/pokemonGUI.java
@@ -116,16 +116,28 @@ public class pokemonGUI extends JFrame {
 	}
 
 	private void setupMenu() {
+		
+		JMenuBar menuBar = new JMenuBar();
+		this.setJMenuBar(menuBar);
+		
+		
 		JMenu menu = new JMenu("Menu");
+		menuBar.add(menu);
 		JMenuItem save = new JMenuItem("Save");
 		menu.add(save);
 		save.addActionListener(new SaveGame());
 		JMenuItem quit = new JMenuItem("Save and Quit");
 		menu.add(quit);
 		quit.addActionListener(new SaveGame());
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.add(menu);
-		this.setJMenuBar(menuBar);
+
+		JMenu items = new JMenu("Items");
+		JMenuItem max_potion = new JMenuItem("MAX_POTION");
+		items.add(max_potion);
+		max_potion.addActionListener(new HealTrainer());
+		menuBar.add(items);
+		//System.out.println("in the menu");
+		
+		
 	}
 
 	public void redraw() {
@@ -156,10 +168,34 @@ public class pokemonGUI extends JFrame {
 		return currentView;
 	}
 
+	private class HealTrainer implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("hello");
+			
+			trainer.heal();
+			if(trainer.isInBattle()){
+				battleview.updatePanel();
+			}else{
+				items.updateTable();
+			}
+			
+			System.out.println("after using the item: " + trainer.getCurrHP());
+			String path = JOptionPane.showInputDialog("The trianer's HP: " + trainer.getCurrHP() +"\n the amount of max_p: "+ trainer.getItemNum(ItemType.MAX_POTION));
+			
+		}
+	}
+	
+	
+	
+	
+	
+	
 	private class SaveGame implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			System.out.println("click save ");
 			if (!trainer.isInBattle()) {
 				int userInput = JOptionPane.showConfirmDialog(null, "Save over existing file?");
 				if (userInput == JOptionPane.YES_OPTION) {
